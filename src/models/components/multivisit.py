78aa97dea08e2406518242b0e063327098d8e72d
amirshamaei <shamaie.amir@gmail.com>
2023-11-17 12:27:31 -0700

    + transformers has been added

--- src/models/components/multivisit.py
+++ src/models/components/multivisit.py
@@ -38,9 +38,9 @@ class CrossAttentionLayer(nn.Module):
 
 
 class MRINet(nn.Module):
-    def __init__(self, dim=256):
+    def __init__(self, dim=512):
         super(MRINet, self).__init__()
-        self.attention=MultiheadAttention(embed_dim=dim, num_heads=4,batch_first=True)
+        # self.attention=MultiheadAttention(embed_dim=dim, num_heads=4,batch_first=True)
         kernel = 16
         # Feature extraction layers for current slice
         self.slice_conv1 = nn.Conv2d(1, dim, kernel_size=kernel, stride=kernel,padding=3)
@@ -48,7 +48,7 @@ class MRINet(nn.Module):
 
         # Feature extraction layers for past scan
         self.volume_conv1 = nn.Conv3d(1, dim, kernel_size=kernel, stride=kernel,padding=(0,3,3))
-        self.volume_conv2 = nn.Conv3d(1, dim, kernel_size=kernel, stride=kernel,padding=(0,3,3))
+        # self.volume_conv2 = nn.Conv3d(1, dim, kernel_size=kernel, stride=kernel,padding=(0,3,3))
 
         # Cross-attention layer
         # self.cross_attention = CrossAttentionLayer(dim*2)
@@ -62,21 +62,25 @@ class MRINet(nn.Module):
         self.dim = dim
         self.norm1 = torch.nn.LayerNorm(dim)
         self.norm2 = torch.nn.LayerNorm(dim)
-        self.norm3 = torch.nn.LayerNorm(dim)
+        # self.norm3 = torch.nn.LayerNorm(dim)
         # self.deconv2 = nn.ConvTranspose2d(dim, 1, kernel_size=7, stride=1, padding=3)
-        self.lin1 = nn.Linear(dim,4*dim)
-        self.lin2 = nn.Linear(4*dim, dim)
-        self.gelu = nn.GELU()
+        # self.lin1 = nn.Linear(dim,4*dim)
+        # self.lin2 = nn.Linear(4*dim, dim)
+        # self.gelu = nn.GELU()
         self.proj = nn.Linear(dim, 16*16)
+        self.transformers = nn.Transformer(batch_first=True)
     def forward(self, x_slice, x_volume):
-        with torch.no_grad():
-            key = self.slice_conv1(x_slice.unsqueeze(1)).view(x_slice.shape[0],self.dim,-1).transpose(1,2)
-            query = self.volume_conv1(x_volume.unsqueeze(1)).view(x_slice.shape[0],self.dim,-1).transpose(1,2)
-            # value = self.volume_conv2(x_volume.unsqueeze(1))
-            key,query = self.norm1(key),self.norm2(query)
-        attn_output, _ = self.attention(key, query, query)
-        attn_output = self.lin2(self.gelu(self.lin1(self.norm3(attn_output))))
-        attn_output = attn_output
+        # with torch.no_grad():
+        key = self.slice_conv1(x_slice.unsqueeze(1)).view(x_slice.shape[0],self.dim,-1).transpose(1,2)
+        query = self.volume_conv1(x_volume.unsqueeze(1)).view(x_slice.shape[0],self.dim,-1).transpose(1,2)
+
+        # value = self.volume_conv2(x_volume.unsqueeze(1))
+        key, query = self.norm1(key),self.norm2(query)
+        attn_output = self.transformers(query,key)
+
+        # attn_output, _ = self.attention(key, query, query)
+        # attn_output = self.lin2(self.gelu(self.lin1(self.norm3(attn_output))))
+        # attn_output = attn_output
         z = self.proj(attn_output).reshape(-1, 1, 14*16, 11*16)[:,:,:-6,:-6]
         # z = self.deconv1(attn_output.transpose(1, 2).reshape(-1, self.dim, 14, 11))
         # z = self.unet(z)
@@ -122,7 +126,7 @@ class MultiVisitNet(nn.Module):
                 for param in single_visit_net.parameters():
                     param.requires_grad = False
         # Initialize the multi-visit network
-        self.unet = UnetModel2d(in_channels=2,out_channels=1,num_filters=8,num_pool_layers=2,dropout_probability=0)
+        self.unet = UnetModel2d(in_channels=156,out_channels=1,num_filters=8,num_pool_layers=2,dropout_probability=0)
 
         self.multi_visit_net =  MRINet()
 
@@ -134,8 +138,8 @@ class MultiVisitNet(nn.Module):
         multi_visit_output = self.multi_visit_net(output_image,x['img_pre'])
         # plt.imshow(output_image.cpu().detach()[0, :, :])
         # plt.title(x['metadata']["File name"])
-        # plt.show()+ 0*multi_visit_output
-        return output_image + self.unet(torch.stack([output_image,multi_visit_output],dim=1)).squeeze(), output_kspace, target_img
+        # plt.show()+ 0*multi_visit_output #+ self.unet(x['img_pre']).squeeze()
+        return output_image+multi_visit_output, output_kspace, target_img
 
 # Example usage:
 # # Define the single-visit and multi-visit networks (they should be instances of nn.Module with the same input/output dimensions)
